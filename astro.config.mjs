354c1c8ebeaa7b559974e61c944819286afcd9b3
José F. Reyes-Santana <jose.reyes@prswservices.com>
2026-07-06 17:07:39 -0400

    More tests with direct root domain (6)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import starlightPageActions from "starlight-page-actions";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://prsws.github.io',
-	base: '/ai4ablog',
+    site: 'https://ai4aging.org',
+	base: '/',
     fonts: [
       {
         name: 'Roboto',
