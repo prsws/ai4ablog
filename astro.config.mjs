5ab92029a7d1318ad83a2e189d1ec67bad5f94c7
José F. Reyes-Santana <jose.reyes@prswservices.com>
2026-09-16 12:51:04 -0400

    Continuing cleanup 39: added position paper ai-in-medicine-and-pepa.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -83,6 +83,7 @@ export default defineConfig({
                     { label: 'Manifesto', slug: 'general/manifesto' },
                     { label: 'Use Cases', slug: 'general/usecases' },
                     { label: 'A Holistic View', slug: 'general/holisticview' },
+                    { label: 'AI in Medicine and Pepa', slug: 'general/ai-in-medicine-and-pepa' },
                     { label: '5min PowerPoint', slug: 'general/5minpowerpoint' },
                     { label: 'TL;DR Build your 1st Pepa', slug: 'guides/buildyour1stpepa' },
                 ],
